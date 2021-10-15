7656d825841458d1c091aff021c60a2d5de2010c
Mohammed Ali Al-Hassan <ali.alhassan30@gmail.com>
2021-10-15 04:47:24 +0000

    Parameterized students.js and teachers.js

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-
+// Pre- parameterized 
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -7,6 +7,8 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+//Pre-parameterized 
+/*
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
@@ -18,5 +20,27 @@ LIMIT ${process.argv[3] || 5};
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
-}).catch(err => console.error('query error', err.stack));
+}).catch(err => console.error('query error', err.stack)); */
+
+//Post Parameterized
+const limit = process.argv[3] || 5;
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+pool
+.query(queryString,values)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
+
 
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,6 +7,28 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+/* Pre-parameterized
+const cohortName = process.argv[2] || 'JUL02';
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY teacher;
+`;
+
+pool
+.query(queryString, [cohortName])
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+})
+.catch(err => console.error('query error', err.stack)); */
+
+//Post parameterized
 const cohortName = process.argv[2] || 'JUL02';
 const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
